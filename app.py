195cb346f1a57bb02e484582b845bcb1272aefc4
RuaraidhCurr <40209895@live.napier.ac.uk>
2018-10-29 16:10:03 +0000

    email instead of username

--- app.py
+++ app.py
@@ -37,11 +37,11 @@ def query_db(query, args=(), one=False):
     return (rv[0] if rv else None) if one else rv
 
 
-def verified_login(username, password):
+def verified_login(useremail, password):
     with sql.connect("cw2.db") as db:
         cur = db.cursor()
-        find_user = ("select * from users where username = ? and password = ?")
-        cur.execute(find_user, [username, password])
+        find_user = ("select * from users where useremail = ? and password = ?")
+        cur.execute(find_user, [useremail, password])
         data = cur.fetchone()
         if data:
             return True
@@ -49,12 +49,12 @@ def verified_login(username, password):
             return False
 
 
-def new_user(username):
+def new_user(useremail):
     with sql.connect("cw2.db") as db:
         userData = None
         cur = db.cursor()
-        findUser = ("select * from users where username = ?")
-        cur.execute(findUser, [username])
+        findUser = ("select * from users where useremail = ?")
+        cur.execute(findUser, [useremail])
         userData = cur.fetchall()
         # checks and sees if there is any userdata matching
         if userData:
@@ -62,18 +62,17 @@ def new_user(username):
         else:
             return False
 
-# create account url
-
 @app.route("/")
 @app.route("/start/")
 @app.route("/main/")
 def route():
-    if "username" in session:
+    if "useremail" in session:
         return redirect(url_for('home'))
     else:
         flash("Please Log in or register new user account")
         return redirect(url_for("login"))
 
+# create account url
 @app.route("/registeruser/")
 @app.route("/newuser/")
 @app.route("/createnewuser/")
@@ -85,7 +84,7 @@ def register():
 def registeruser():
     db = get_db()
     if request.method == "POST":
-        username = request.form["username"]
+        useremail = request.form["useremail"]
         password = request.form["password"]
         verPassword = request.form["verPassword"]
         nameCombo = request.form["nameCombo"]
@@ -93,15 +92,15 @@ def registeruser():
         firstname = nameCombo[0]
         lastname = nameCombo[-1]
         if password == verPassword:
-            if new_user(username):
-                flash("Username allready Found")
+            if new_user(useremail):
+                flash("account using that email has allready been made")
                 # redirects user to the login page
                 return redirect(url_for("login"))
             else:
                 with sql.connect("cw2.db") as db:
                     cur = db.cursor()
                     cur.execute(
-                        "insert into users (username, password, firstname, lastname) values (?,?,?,?)", (username, password, firstname, lastname))
+                        "insert into users (useremail, password, firstname, lastname) values (?,?,?,?)", (useremail, password, firstname, lastname))
                     db.commit()
                     flash("User account Registered!     Please Log in")
             return redirect(url_for("login"))
@@ -114,20 +113,18 @@ def registeruser():
     return redirect(url_for("login"))
 
 #Log in screen
-
-
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         # Checks to see if the sucessful login params were met
-        if verified_login(request.form["username"], request.form["password"]):
+        if verified_login(request.form["useremail"], request.form["password"]):
             # stores flash message for sucseful login
             flash("Login Succseful")
-            # stores username in username session
-            session["username"] = request.form["username"]
+            # stores useremail in useremail session
+            session["useremail"] = request.form["useremail"]
             return redirect(url_for("home"))
         else:
-            flash("Invalid username and/or passwords")
+            flash("Invalid useremail and/or passwords")
     return render_template("login.html")
 
 @app.route("/main/logout/")
@@ -135,7 +132,7 @@ def login():
 @app.route("/logout")
 def logout():
     # deletes the session
-    session.pop("username", None)
+    session.pop("useremail", None)
     # redirects to login
     return redirect(url_for("login"))
 
@@ -143,10 +140,10 @@ def logout():
 @app.route("/Home/")
 @app.route("/home")
 def home():
-    # if the session has a username stores in warningit
-    if "username" in session:
-        # gets username from session
-        return render_template("home.html", username=session["username"])
+    # if the session has a useremail stores in warningit
+    if "useremail" in session:
+        # gets useremail from session
+        return render_template("home.html", useremail=session["useremail"])
     else:
         flash("Please log in or create account!")
         return redirect(url_for("login"))
@@ -155,7 +152,7 @@ def home():
 
 @app.errorhandler(404)
 def page_not_found(error):
-    if "username" in session:
+    if "useremail" in session:
         flash("Sorry that page was not found")
         return redirect(url_for('home'))
     else:
